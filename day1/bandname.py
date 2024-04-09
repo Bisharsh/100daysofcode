572948f4d574c87bf2c5cc4797fe1c6662b23b48
Harshit Bisht <harshitbisht@Harshits-Air.lan>
2024-04-09 14:39:28 +0530

    pushing changes

---
+++ day1/bandname.py
@@ -0,0 +1,7 @@
+def bandNameGenerator():
+    print("Welcome to the Band Name Generator")
+    cityName = input("name of city?\n")
+    petName = input("Pet name?\n")
+    print("your band name could be " +cityName+ " " +petName)
+
+bandNameGenerator()
