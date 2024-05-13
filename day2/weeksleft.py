bcfbc0280119e2a6aa0dfb0b29a0efb93814a51b
Harshit Bisht <harshitbisht@Harshits-Air.lan>
2024-05-13 20:15:47 +0530

    updated day2

---
+++ day2/weeksleft.py
@@ -0,0 +1,7 @@
+current_age = int(input("please enter your current age: "))
+
+max_age = 90
+
+years_left = max_age - current_age
+
+print("number of weeks that you have left with are:", years_left*52)
