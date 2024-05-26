a3e8665684be6294e18c6b6f1cce08df6394500d
Harshit Bisht <harshitbisht@Harshits-Air.lan>
2024-05-27 00:55:26 +0530

    removed redundant file

--- 100daysofcode/day1/bandname.py
+++
@@ -1,7 +0,0 @@
-def bandNameGenerator():
-    print("Welcome to the Band Name Generator")
-    cityName = input("name of city?\n")
-    petName = input("Pet name?\n")
-    print("your band name could be " +cityName+ " " +petName)
-
-bandNameGenerator()
